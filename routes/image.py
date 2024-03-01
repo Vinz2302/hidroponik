2b64a075bb8c96c1a348f54995d6dec7f61f5c77
Vinz <vincentkosasih2302@gmail.com>
2024-03-01 17:07:05 +0700

    define routes

---
+++ routes/image.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+from firebaseConn import generate_download_url
+
+image_routes = Blueprint('image', __name__)
+
+@image_routes.route('/image', methods=['POST'])
+def get_image_post():
+    data = request.get_json()
+    if 'file_path' not in data:
+        return jsonify({'error': 'File path not provided'}), 400
+    
+    file_path = data['file_path']
+    download_url = generate_download_url(file_path)
+    return jsonify({'download_url': download_url})
+
+@image_routes.route('/image', methods=['GET'])
+def get_image():
+    file_path = request.args.get('file_path')
+    if not file_path:
+        return jsonify({'error': 'File path not provided in query param'}), 400
+    
+    download_url = generate_download_url(file_path)
+    return jsonify({'download_url': download_url})
--- server-flask.py
+++ server-flask.py
@@ -1,40 +1,20 @@
 from flask import Flask, request, jsonify
 from firebaseConn import generate_download_url
 from flask_cors import CORS
+from routes.image import image_routes
+
+PORT = 8000
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/arduino', methods=['GET'])
-def test_arduino():
-    from action.instruction import arduino_control
-    arduino_control()
-    return jsonify({'message': 'Success'})
+# @app.route('/arduino', methods=['GET'])
+# def test_arduino():
+#     from action.instruction import arduino_control
+#     arduino_control()
+#     return jsonify({'message': 'Success'})
 
-@app.route('/image', methods=['POST'])
-def get_download_url_post():
-    # Get the file_path from the body
-    data = request.get_json()
-
-    if 'file_path' not in data:
-        return jsonify({'error': 'File path not provided'}), 400
-
-    file_path = data['file_path']
-    download_url = generate_download_url(file_path)
-
-    return jsonify({'download_url': download_url})
-
-@app.route('/image', methods=['GET'])
-def get_download_url():
-    #Get the file_path from the param
-    file_path = request.args.get('file_path')
-
-    if not file_path:
-        return jsonify({'error': 'File path not provided in query parameter'}), 400
-    
-    download_url = generate_download_url(file_path)
-    return jsonify({'download_url': download_url})
-
+app.register_blueprint(image_routes)
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=PORT)
